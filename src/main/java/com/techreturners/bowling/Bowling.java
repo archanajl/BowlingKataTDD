5f2f4e1f38d65fc734ae103333a6d14d49868de3
Archana Janarthanan <archanajl@gmail.com>
2022-04-05 21:24:27 +0100

    Test checkCalculateScoreAllRollswithvalidNumbers passed

--- src/main/java/com/techreturners/bowling/Bowling.java
+++ src/main/java/com/techreturners/bowling/Bowling.java
@@ -4,8 +4,11 @@ public class Bowling {
 
     public int calculateScore(String scoreboard){
         int score =  0;
-        for (int i=0; i< scoreboard.length();i++){
-            score += Character.getNumericValue(scoreboard.charAt(i));
+
+        String onlyScores = scoreboard.replaceAll("\\s+","");
+
+        for (int i=0; i< onlyScores.length();i++){
+            score += Character.getNumericValue(onlyScores.charAt(i));
         }
         return score;
     }
